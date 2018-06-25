a5209d35cdcb7de6718c4a1e038922185179d6e0
Max Payne <forgge@gmail.com>
2018-06-25 23:02:48 +0300

    support for the new theCore configurator

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "target.hpp"
-
 // For console
 #include <ecl/iostream.hpp>
 // For semaphore functionality
 #include <ecl/thread/semaphore.hpp>
 // For button class
 #include <platform/exti_manager.hpp>
+// For generated code
+#include <aux/generated.hpp>
 
 extern "C"
 void board_init()
